237e58a57842d224ba5b1ab80467975dda4bf9ac
jnngrlo <kje894@gmail.com>
2025-05-25 17:02:37 +0900

    [0525] test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>안녕하세요</h1>
+      <div>저는 개쩌는 개발자입니다.</div>
+      <div>솔직히 저만큼 많은 경험을 한 개발자? 없을겁니다.</div>
+      <div>졸업하자마자 회사 입사하고, 사업하고, 말아먹고 알바하고</div>
+      <div>공백이 있을지언정, 쉰 적 없습니다 ㅋㅋ;;</div>
     </div>
   );
 }
